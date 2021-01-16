f3ca6b1e48b5166953838487769644223f2dc366
Lenni vH <lennivh24@gmail.com>
2021-01-16 15:57:41 +0100

    lights v2

--- Lights/Lights.ino
+++ Lights/Lights.ino
@@ -7,29 +7,35 @@
 
 unsigned char brightness = 50;
 
-const unsigned long minute = ((long) 1000) * 60; // 1 min in ms
+const unsigned long second = ((long) 1000); // 1 s in ms
+const unsigned long minute = second * 60; // 1 min in ms
 const unsigned long hour = minute * 60; // 1 hr in ms
 
-const unsigned long fadeStart = 1 * hour;
-const unsigned long fadePerMinute = 5;
+const unsigned long fadeStart = 2 * hour;
+const unsigned long millisPerFade = 30 * second;
+
+unsigned long nextFade = 0;
 
 void setup() {
   pinMode(BT1, INPUT);
   pinMode(BT2, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (millis() < fadeStart) {
-    delay(10);
-    if (digitalRead(BT1) == HIGH && brightness < 255)
-      brightness += 1;
-    if (digitalRead(BT2) == HIGH && brightness > 0)
-      brightness -= 1;
-  } else {
-    delay(minute);
-    brightness = max(brightness - fadePerMinute, 0);
+  delay(10);
+  if (digitalRead(BT1) == HIGH && brightness < 255)
+    brightness += 1;
+  if (digitalRead(BT2) == HIGH && brightness > 0)
+    brightness -= 1;
+  if (nextFade != -0 && millis() > nextFade) {
+    brightness--;
+    Serial.println(brightness);
+    nextFade = millis() + millisPerFade;
+  } else if (nextFade == 0 && millis() > fadeStart) {
+    nextFade = millis() + millisPerFade;
   }
-
+  
   analogWrite(LT1, brightness);
   analogWrite(LT2, brightness);
   analogWrite(LT3, brightness);
